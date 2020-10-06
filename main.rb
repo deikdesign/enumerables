54ad61bdf0abaadec3f5dfba618fbda8e23ce8fb
deikdesign <deikdesign@gmail.com>
2020-10-06 19:15:38 +0300

    Disable rubocop style

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
 require_relative './lib/enumerables.rb'
 
-#[1, 2, 3, 4].my_each { |num| puts num}
+[1, 2, 3, 4].my_each { |num| puts num}
 
-#[1, 2, 3, 4].my_each_with_index { |val, ind| puts "for #{ind} index- value is #{val}"}
+[1, 2, 3, 4].my_each_with_index { |val, ind| puts "for #{ind} index- value is #{val}"}
 
-#puts [1, 2, 3, 4].my_select { |num| num.even? }
+puts [1, 2, 3, 4].my_select { |num| num.even? }
 
-#puts [1, 2, 3, 5].my_all? { |num| num > 0 }
+puts [1, 2, 3, 5].my_all? { |num| num > 0 }
 
-#puts [1, 2, 3, 5].my_any? multiply_els{ |num| num == 3 }
+puts [1, 2, 3, 5].my_any? multiply_els{ |num| num == 3 }
 
-#puts [1, 2, 3, 5].my_none? { |num| num < 0 }
+puts [1, 2, 3, 5].my_none? { |num| num < 0 }
 
-#puts [1, 2, 3, 3, 4].my_count {|x| x % 2 == 0 }
+puts [1, 2, 3, 3, 4].my_count {|x| x % 2 == 0 }
 
-#puts ['h', 'm', 'a', 'y'].my_map { |x| x.capitalize}
+puts ['h', 'm', 'a', 'y'].my_map { |x| x.capitalize}
 
-#puts [1, 2, 3, 4, 5].my_inject { |sum, num| sum + num }
+puts [1, 2, 3, 4, 5].my_inject { |sum, num| sum + num }
 
-#puts multiply_els([2, 4, 5])
+puts multiply_els([2, 4, 5])
