84b0a69738846a812510a0ea6d5662ecde4fa9b3
hamayun-cpu <hamayun.waheed1999@gmail.com>
2020-10-07 05:01:03 +0500

    fixed linter errors

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require_relative './lib/enumerables.rb'
 
-[1, 2, 3, 4].my_each { |num| puts num}
+[1, 2, 3, 4].my_each { |num| puts num }
 
-[1, 2, 3, 4].my_each_with_index { |val, ind| puts "for #{ind} index- value is #{val}"}
+[1, 2, 3, 4].my_each_with_index { |val, ind| puts "for #{ind} index- value is #{val}" }
 
 puts [1, 2, 3, 4].my_select { |num| num.even? }
 
